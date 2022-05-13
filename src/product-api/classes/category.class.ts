13a4c31a17a9434f1d1e7833583aeec1f7d68412
ATJSH <letaem77@gmail.com>
2022-05-13 12:27:20 +0900

    fix: remove unused decorator

--- src/product-api/classes/category.class.ts
+++ src/product-api/classes/category.class.ts
@@ -1,4 +1,4 @@
-import { Expose, Type } from "class-transformer";
+import { Type } from "class-transformer";
 import {
   ArrayMinSize,
   IsBoolean,
@@ -15,31 +15,24 @@ export class CategoryCounterContainer {
 
 export class Category {
   @IsNumber()
-  @Expose({ name: "category_no" })
   category_no: number;
 
   @IsString()
-  @Expose({ name: "category_name" })
   category_name: string;
 
   @IsNumber()
-  @Expose({ name: "category_depth" })
   category_depth: number;
 
   @IsString()
-  @Expose({ name: "root_category_no" })
   root_category_no: string;
 
   @IsString()
-  @Expose({ name: "parent_category_no" })
   parent_category_no: string | null;
 
   @IsObject()
-  @Expose({ name: "full_category_no" })
   full_category_no: Record<string, any>;
 
   @IsBoolean()
-  @Expose({ name: "use_yn" })
   use_yn: boolean;
 }
 
--- src/product-api/classes/product.class.ts
+++ src/product-api/classes/product.class.ts
@@ -1,4 +1,4 @@
-import { Expose, Type } from "class-transformer";
+import { Type } from "class-transformer";
 import {
   ArrayMinSize,
   IsBoolean,
@@ -15,47 +15,36 @@ export class ProductCounterContainer {
 
 export class Product {
   @IsNumber()
-  @Expose({ name: "product_no" })
   product_no: number;
 
   @IsString()
-  @Expose({ name: "product_name" })
   product_ame: string;
 
   @IsNumber()
-  @Expose({ name: "product_price" })
   product_price: number;
 
   @IsNumber()
-  @Expose({ name: "product_discount_price" })
   product_discount_price: number;
 
   @IsBoolean()
-  @Expose({ name: "display_yn" })
   display_yn: boolean;
 
   @IsBoolean()
-  @Expose({ name: "soldout_yn" })
   soldout_yn: boolean;
 
   @IsBoolean()
-  @Expose({ name: "sell_yn" })
   sell_yn: boolean;
 
   @IsString()
-  @Expose({ name: "product_thumbnail_image_url" })
   product_thumbnail_image_url: string;
 
   @IsString()
-  @Expose({ name: "product_detail_url" })
   product_detail_url: string;
 
   @IsDateString()
-  @Expose({ name: "product_register_date" })
   product_register_date: Date;
 
   @IsDateString()
-  @Expose({ name: "product_modify_date" })
   product_modify_date: Date | null;
 }
 
